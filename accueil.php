c129a3360e8c7ce70baaec64fa1d3ce89acfb028
Timour <122935633+TimourElonMusk@users.noreply.github.com>
2023-06-15 13:42:34 +0200

    Create accueil.php

---
+++ accueil.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <Title>Association Sancyberie - Site Officiel</Title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="./assets/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./assets/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./assets/favicon-16x16.png">
+    <link rel="manifest" href="./assets/site.webmanifest">
+    <link rel="mask-icon" href="./assets/safari-pinned-tab.svg" color="#5daaff">
+    <link rel="shortcut icon" href="./assets/favicon.ico">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <meta name="msapplication-TileColor" content="#5daaff">
+    <meta name="msapplication-config" content="./assets/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+    <meta charset="UTF-8">
+    <meta name="keywords" content="Francorusse, Culture, Art, Musique, Danse, Voyages, France, Russie, Jbanov">
+    <meta name="description" content="Si vous êtes amateurs de Musique, de Danse, d'Art en général et de Voyages, vous êtes ici chez vous !">
+    <meta name="author" content="Roman Jbanov">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/430603bab2.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <?php include 'includes/header.php';?>
+
+    <main>
+        <section>
+            <div class="container">
+                <picture class="picture__container">
+                    <source srcset="./assets/img/sancyberie_tableau.webp" type="image/webp">
+                    <source srcset="./assets/img/sancyberie_tableau.jpg" type="image/jpg">
+                    <img id="painting" src="./assets/img/sancyberie_tableau.png" alt="Tableau Sancybérie" type="image/png">
+                </picture>
+                <div class="centered-div-text">
+                    <p class="paragraph calligraphy-letter">
+                        <span class="centered-text calligraphy-letter">Bonjour à toutes et à tous,</span>
+                        <br>
+                        Si vous êtes amateurs de Musique, de Danse, d'Art en général et de Voyages, vous êtes ici
+                        chez vous !
+                        <br>
+                        Nous avons constitué l'association "Sancybérie" en Avril 2009 avec pour
+                        but de faire découvrir la Culture Russe sous toutes ses formes dans notre
+                        belle région d'Auvergne et d'exporter la Culture Française en Russie.
+                        <br>
+                        <span class="bold-text calligraphy-letter">2010 : Année Croisée France-Russie !</span>
+                        <br>
+                        Voici une belle idée, pleine d'humanité, de curiosité et de générosité. Nous
+                        avons immédiatement été ferraillés par l'envie d'être acteurs de cette
+                        grande manifestation d'ouverture sur les arts et les peuples.
+                        <br>
+                        De part nos origines ou nos parcours professionnels, nous avons eu la
+                        chance de découvrir ce grand pays qu'est la Russie, qui est aujourd'hui
+                        présent à jamais dans nos cœurs et dans nos esprits.
+                        <br>
+                        Nous espérons que vous aussi, soit en visitant ce site, soit en étant
+                        présents lors de nos manifestations, vous tomberez sous le charme de ce
+                        pays et de son peuple.
+                        <br>
+                        Nous vous souhaitons en tout cas une bonne visite, et on espère que
+                        nos projets vous plairont et qu'ils aboutiront.
+                        <br>
+                        <span class="centered-text calligraphy-letter">Amicalement.</span>
+                        <br>
+                        <span class="text-author centered-text calligraphy-letter">Roman Jbanov, Président de l'Association, chargé du développement international</span>
+                    </p>
+                </div>   
+            </div>
+        </section>
+    </main>
+
+    <footer>
+        <div class="social">
+            <a href="https://www.facebook.com/groups/162255880971788/" target="_blank"><i class="icon fa-brands fa-facebook-f"></i></a>
+            <a href="https://www.youtube.com/channel/UCrPIdVbiyYxuocfEGNAcldg" target="_blank"><i class="icon fa-brands fa-youtube"></i></a>
+            <a href="https://vk.com/club72133984" target="_blank"><i class="icon fa-brands fa-vk"></i></a>
+        </div>
+        <ul class="list-inline">
+                <li class="list-inline-item"><a href="accueil.html">Accueil</a></li>
+                <li class="list-inline-item"><a href="#">Administration</a></li>
+                <li class="list-inline-item"><a href="#">Statuts</a></li>
+                <li class="list-inline-item"><a href="#">Adhésion</a></li>
+                <li class="list-inline-item"><a href="#">Projets</a></li>
+                <li class="list-inline-item">Festivals/Concours</li>
+                <li class="list-inline-item"><a href="#">Historique</a></li>
+                <li class="list-inline-item"><a href="#">Contact</a></li>
+                <li class="list-inline-item"><a href="#">Liens</a></li>
+        </ul>
+        <p class="copyright">© Sancybérie - Tous droits réservés</p>
+    </footer>
+</body>
+</html>
