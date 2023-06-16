c65344c2c552df24a43cb1d0acbffee1104107bf
Timour <122935633+TimourElonMusk@users.noreply.github.com>
2023-06-16 22:44:04 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,27 +1,25 @@
 <header>
     <img class="header__banner no-select" src="./assets/header.png" alt="banner">
     <nav>
-        <ul class="menu">
-            <div class="menu__parts">
-                <li class="menu__part"><a class="menu__part__link" href="accueil.html">Accueil</a></li>
-                <li class="menu__part"><a class="menu__part__link" href="#">Administration</a></li>
-                <li class="menu__part"><a class="menu__part__link" href="#">Statuts</a></li>
-                <li class="menu__part"><a class="menu__part__link" href="#">Adhésion</a></li>
-                <li class="menu__part"><a class="menu__part__link" href="#">Projets</a></li>
-                <li class="menu__part scrolling-menu"><a class="menu__part__link" href="#">Festivals/Concours&ensp;</a>
-                    <ul class="submenu">
-                        <li class="submenu__part"><a href="festival_2009.html">Festival 2009</a></li>
-                        <li class="submenu__part"><a href="festival_2010.html">Festival 2010</a></li>
-                        <li class="submenu__part"><a href="festival_2011.html">Festival 2011</a></li>
-                        <li class="submenu__part"><a href="festival_2012.html">Festival 2012</a></li>
-                        <li class="submenu__part"><a href="festival_2013.html">Festival 2013</a></li>
-                        <li class="submenu__part"><a href="concours_2017.html">Concours 2017</a></li>
-                    </ul>
-                </li>
-                <li class="menu__part"><a class="menu__part__link" href="#">Historique</a></li>
-                <li class="menu__part"><a class="menu__part__link" href="#">Contact</a></li>
-                <li class="menu__part"><a class="menu__part__link" href="#">Liens</a></li>
-            </div>
+        <ul>
+            <li><a href="accueil.html">Accueil</a></li>
+            <li><a href="#">Administration</a></li>
+            <li><a href="#">Statuts</a></li>
+            <li><a href="#">Adhésion</a></li>
+            <li><a href="#">Projets</a></li>
+            <li class="scrolling-menu"><a href="#">Festivals/Concours&ensp;</a>
+                <ul class="submenu">
+                    <li><a href="festival_2009.html">Festival 2009</a></li>
+                    <li><a href="festival_2010.html">Festival 2010</a></li>
+                    <li><a href="festival_2011.html">Festival 2011</a></li>
+                    <li><a href="festival_2012.html">Festival 2012</a></li>
+                    <li><a href="festival_2013.html">Festival 2013</a></li>
+                    <li><a href="concours_2017.html">Concours 2017</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Historique</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Liens</a></li>
         </ul>
     </nav>
 </header>
