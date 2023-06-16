60776649bb6521d422a4176af02da5c6358dfbd6
Timour <122935633+TimourElonMusk@users.noreply.github.com>
2023-06-16 17:03:28 +0200

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+<footer>
+    <div class="social">
+        <a href="https://www.facebook.com/groups/162255880971788/" target="_blank"><i class="icon fa-brands fa-facebook-f"></i></a>
+        <a href="https://www.youtube.com/channel/UCrPIdVbiyYxuocfEGNAcldg" target="_blank"><i class="icon fa-brands fa-youtube"></i></a>
+        <a href="https://vk.com/club72133984" target="_blank"><i class="icon fa-brands fa-vk"></i></a>
+    </div>
+    <ul class="list-inline">
+            <li class="list-inline-item"><a href="accueil.html">Accueil</a></li>
+            <li class="list-inline-item"><a href="#">Administration</a></li>
+            <li class="list-inline-item"><a href="#">Statuts</a></li>
+            <li class="list-inline-item"><a href="#">Adhésion</a></li>
+            <li class="list-inline-item"><a href="#">Projets</a></li>
+            <li class="list-inline-item">Festivals/Concours</li>
+            <li class="list-inline-item"><a href="#">Historique</a></li>
+            <li class="list-inline-item"><a href="#">Contact</a></li>
+            <li class="list-inline-item"><a href="#">Liens</a></li>
+    </ul>
+    <p class="copyright">© Sancybérie - Tous droits réservés</p>
+</footer>
