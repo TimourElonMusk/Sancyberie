c489e67241252871c22218e5abc65267bfd51d91
Timour <122935633+TimourElonMusk@users.noreply.github.com>
2023-07-01 21:34:34 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,27 +1,29 @@
 <header>
-    <img class="header__banner no-select" src="./assets/header.png" alt="banner">
-
-    <nav id="topnav">
-        <ul>
-            <li><a href="accueil.html">Accueil</a></li>
-            <li><a href="#">Administration</a></li>
-            <li><a href="#">Statuts</a></li>
-            <li><a href="#">Adhésion</a></li>
-            <li><a href="#">Projets</a></li>
-            <li class="scrolling-menu"><a href="#">Festivals/Concours&ensp;</a>
-                <ul class="submenu">
-                    <li><a href="festival_2009.html">Festival 2009</a></li>
-                    <li><a href="festival_2010.html">Festival 2010</a></li>
-                    <li><a href="festival_2011.html">Festival 2011</a></li>
-                    <li><a href="festival_2012.html">Festival 2012</a></li>
-                    <li><a href="festival_2013.html">Festival 2013</a></li>
-                    <li><a href="concours_2017.html">Concours 2017</a></li>
+        <img class="header__banner no-select" src="./assets/header.png" alt="banner">
+    
+        <nav>
+            <div class="topnav" id="myTopnav">
+                <ul>
+                    <li><a href="accueil.html">Accueil</a></li>
+                    <li><a href="#">Administration</a></li>
+                    <li><a href="#">Statuts</a></li>
+                    <li><a href="#">Adhésion</a></li>
+                    <li><a href="#">Projets</a></li>
+                    <li class="scrolling-menu"><a href="#">Festivals/Concours&ensp;</a>
+                        <ul class="submenu">
+                            <li><a href="festival_2009.html">Festival 2009</a></li>
+                            <li><a href="festival_2010.html">Festival 2010</a></li>
+                            <li><a href="festival_2011.html">Festival 2011</a></li>
+                            <li><a href="festival_2012.html">Festival 2012</a></li>
+                            <li><a href="festival_2013.html">Festival 2013</a></li>
+                            <li><a href="concours_2017.html">Concours 2017</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Historique</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Liens</a></li>
+                    <a href="javascript:void(0);" class="nav-icon" onclick="responsiveNav">&#9776</a></li>
                 </ul>
-            </li>
-            <li><a href="#">Historique</a></li>
-            <li><a href="#">Contact</a></li>
-            <li><a href="#">Liens</a></li>
-            <a href="javascript:void(0);" class="nav-icon" onclick="myFunction">&#9776</a></li>
-        </ul>
-    </nav>
-</header>
+            </div>
+        </nav>
+    </header>
