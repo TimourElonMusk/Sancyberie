a3914bc57eec4dc6b42d39a17121a9de921febbf
Timour <122935633+TimourElonMusk@users.noreply.github.com>
2023-10-12 15:34:35 +0200

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-/* Toggle between adding and removing the "responsive-nav" class to topnav when the user clicks on the icon */
-function responsiveNav() {
-var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive-nav";
-    } else {
-        x.className = "topnav";
-    }
-}
